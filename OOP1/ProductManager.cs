d8e3c81eae057ecd33de4d0bcf63ca4828f032a0
ZEKI OZDEN <mehmetzekiozden@hotmail.com>
2021-12-14 13:58:18 +0300

    Add project files.

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    //ProductManager Ürün ile ilgili operasyonlar bulunur
+    // Ekleme,Silme,Güncelleme,Listeleme,Arama,Filtreleme
+    //Operasyonlar içerir
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName+"Eklendi");
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName+"güncellendi");
+        }
+    }
+}
